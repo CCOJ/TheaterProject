3ba7f4178303eeb70b830d06d99d8ea9fb3fd91a
CCOJ <randynguyen09@hotmail.com>
2018-02-20 01:33:45 -0600

    Client: Getters, Setters, Serializable
    Note: Unique ID needs to be done, maybe via another class

--- src/ClientInfo/Client.java
+++ src/ClientInfo/Client.java
@@ -1,4 +1,5 @@
 package ClientInfo;
+import java.io.Serializable;
 
 /**
  * Client holds information about name, address, and phone number.
@@ -7,15 +8,16 @@ package ClientInfo;
  * 
  * Current iteration states the balance will remain 0 for now.
  */
-public class Client {
+public class Client implements Serializable{
 
+	private static final long serialVersionUID = 1L;
 	private String clientName, address, phoneNumber;
-	private int clientID;
+	private int clientId;
 	private double balance;
 	
 	/**
-	 * Client constructor that asks for the given input of name, address, and phone number.
-	 * Then, the unique id is generated and balance gets set to 0.
+	 * A client object is created with given inputs, unique ID, and balance set to 0.
+	 * Client will be added to clientList once created.
 	 * 
 	 * @param clientName Name of client
 	 * @param address Client's address
@@ -25,13 +27,83 @@ public class Client {
 		this.clientName = clientName;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
-		this.clientID = 0; //TODO: Generate unique ID
+		this.clientId = 0; //TODO: Generate unique ID
 		this.balance = 0;
 	}
 	
 	/**
-	 * Change the client's balance
-	 * 
+	 * Gets name of client's Name
+	 * @return clientName
+	 */
+	public String getClientName() {
+		return clientName;
+	}
+	
+	/**
+	 * Gets client's address
+	 * @return address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	
+	/**
+	 * Get client's phone number
+	 * @return phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	/**
+	 * Get client's ID
+	 * @return clientID
+	 */
+	public int getClientId() {
+		return clientId;
+	}
+	
+	/**
+	 * Get client's balance
+	 * @return balance
+	 */
+	public double getBalance() {
+		return balance;
+	}
+	
+	/**
+	 * Set client's name
+	 * @param clientName Client's address
+	 */
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	/**
+	 * Set client's address
+	 * @param address Client's address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	/**
+	 * Set client's phone number
+	 * @param phoneNumber Client's phone number
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	/**
+	 * Set client's ID (must be unique)
+	 * @param clientId Client's ID
+	 */
+	public void setclientId(int clientId) {
+		this.clientId = clientId;
+	}
+	
+	/**
+	 * Set the client's balance
 	 * @param balance Client's balance
 	 */
 	public void setBalance(double balance) {
